c75d7819400687f31d35d089953541b2eb255b5f
anishhimself <57608042+anishhimself@users.noreply.github.com>
2021-08-16 14:37:57 +0545

    once again

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("",views.home,name="home"),
+    path("about-us/",views.aboutUs,name="aboutUs"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def home(request):
-    return render(request,'new/index.html')
+    return render(request,'home/index.html')
+
+def aboutUs(request):
+    return render(request,'home/about-us.html')
