b5924f2b67f28d95194248f1e85b1be215114799
anishhimself <57608042+anishhimself@users.noreply.github.com>
2021-08-17 22:34:47 +0545

    backend

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path("login/",views.login,name="login"),
-    path("register/",views.login,name="login"),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
+
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
+
 ]
--- home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
-
-def home(request):
-    return render(request,'home/index.html')
-
-def aboutUs(request):
-    return render(request,'home/about-us.html')
