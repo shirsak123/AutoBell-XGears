408a34136bb75cc5b41d7b5a1f0a7359bf16b493
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-08-18 20:52:31 +0545

    Change order to class based views

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="dashboard"),
-    path('orders/',views.orders, name="orders"),
+    path('', views.index, name="dashboard"),
+    path('orders/', views.OrderListView.as_view(), name="orders"),
 ]
 
 app_name = "myadmin"
-
--- myadmin/views.py
+++ myadmin/views.py
@@ -1,19 +1,35 @@
 # External Import
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 
 # Internal Import
 from orders.models import Order
 
+
 def index(request):
     context = {}
     context['segment'] = 'index'
-    return render(request,'myadmin/index.html',context)
+    return render(request, 'myadmin/index.html', context)
 
 
-def orders(request):
-    orders_obj = Order.objects.all()
-    context = {
-        'orders': orders_obj,
-    }
-    print(orders_obj)
-    return render(request,'myadmin/orders.html',context)
+class OrderListView(UserPassesTestMixin, ListView):
+    template_name = "myadmin/orders.html"
+    model = Order
+
+    # Check if the user can access this page
+    # Declare permission who can access this page
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            return self.request.user.is_staff
+        return True
