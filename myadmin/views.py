900ad7bb4bb28437ec5e763beb597def49408e5b
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-08-18 17:31:38 +0545

    Render Admin dashboard for test

--- myadmin/views.py
+++ myadmin/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {}
+    context['segment'] = 'index'
+    return render(request,'myadmin/index.html',context)
