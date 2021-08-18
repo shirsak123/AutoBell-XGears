c685765f2b17b0f9244e8f2a3e1570ac109ff10a
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-08-18 18:18:02 +0545

    Show Orders in table in myadmin

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="dashboard")
+    path('',views.index, name="dashboard"),
+    path('orders/',views.orders, name="orders"),
 ]
 
-
+app_name = "myadmin"
 
--- myadmin/views.py
+++ myadmin/views.py
@@ -1,8 +1,19 @@
+# External Import
 from django.shortcuts import render
 
-# Create your views here.
+# Internal Import
+from orders.models import Order
 
 def index(request):
     context = {}
     context['segment'] = 'index'
     return render(request,'myadmin/index.html',context)
+
+
+def orders(request):
+    orders_obj = Order.objects.all()
+    context = {
+        'orders': orders_obj,
+    }
+    print(orders_obj)
+    return render(request,'myadmin/orders.html',context)
