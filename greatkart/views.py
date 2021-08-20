6954d0c08a2473baddbb277efc1b5484a98fc1f3
Prabesh Pathak <prabeshpathak50@gmail.com>
2021-08-20 14:18:03 +0545

    added popular, latest and sale

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from store.models import Product
 
+#Sorting Method/Function
+def getavg(obj):
+    return obj.averageReview()
+
 def home(request):
-    products = Product.objects.all().filter(is_available=True)
+    qs = Product.objects.filter()
+    sale = qs[:6]
     latest = Product.objects.all().filter(is_available=True).order_by('created_date')[:6]
+    mostPopular = sorted(qs,key=getavg,reverse=True)[:6]
 
     context = {
-        'products': products,
-        'latest': latest
+        'products': sale,
+        'latest': latest,
+        'mostPopular': mostPopular
     }
     return render(request, 'home.html', context)
